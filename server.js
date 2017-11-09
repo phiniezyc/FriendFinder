2438e5717bf0dcec2dc289a4ce81e7c59c53d865
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-08 23:10:43 -0500

    server connection to Routes working

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+
+
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
