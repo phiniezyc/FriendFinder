1ff28e34faf436a193dfdc3e48210d37e6181d3c
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-10 01:46:44 -0500

    added some persistant data to the friends.js file so we have objects to compare user input to

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,26 +1,43 @@
 // 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
-// ```json
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
-// ```
 
 
-const friends = [];
+
+
+const friends = [{
+        "name": "Lebron",
+        "photo": "http://media.cleveland.com/cavs_impact/photo/lebron-james-media-dayjpg-df52d9ef28aa19de.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "James",
+        "photo": "http://www.addmorejuice.com/wp-content/uploads/2017/07/James-harden.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+
+];
 
 
 module.exports = friends;
