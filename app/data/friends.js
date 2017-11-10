458e932ced3d20c9ba208bb5af0d9bb91c05f866
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-10 16:20:03 -0500

    updated values to reflect players personalities

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,15 +2,15 @@ const friends = [{
         "name": "Lebron",
         "photo": "http://media.cleveland.com/cavs_impact/photo/lebron-james-media-dayjpg-df52d9ef28aa19de.jpg",
         "scores": [
-            5,
-            1,
             4,
-            4,
-            5,
             1,
             2,
             5,
-            4,
+            2,
+            1,
+            3,
+            3,
+            3,
             1
         ]
     },
@@ -18,63 +18,63 @@ const friends = [{
         "name": "James",
         "photo": "http://www.addmorejuice.com/wp-content/uploads/2017/07/James-harden.jpg",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
+            3,
             2,
-            5,
+            3,
+            2,
             4,
-            1
+            2,
+            1,
+            3,
+            3,
+            3
         ]
     },
     {
         "name": "Giannis",
         "photo": "https://clutchpoints.com/wp-content/uploads/2016/10/giannis-antetokounmpo.jpg",
         "scores": [
-            5,
-            1,
             4,
-            4,
-            5,
-            1,
             2,
             5,
+            5,
+            1,
             4,
-            1
+            5,
+            4,
+            1,
+            3
         ]
     },
     {
         "name": "Steph",
         "photo": "http://betterlivingthroughbeowulf.com/wp-content/uploads/2013/05/Stephen-Curry.jpg",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
             5,
+            2,
+            1,
             4,
-            1
+            1,
+            1,
+            3,
+            3,
+            2
         ]
     },
     {
         "name": "Kevin",
         "photo": "http://s1.firstpost.in/wp-content/uploads/2017/02/Kevin-Durant-NBA-vs-Thunder-AP.jpg",
         "scores": [
-            5,
             1,
-            4,
-            4,
+            1,
+            2,
+            2,
             5,
             1,
             2,
-            5,
-            4,
+            3,
+            2,
             1
         ]
     },
@@ -82,16 +82,16 @@ const friends = [{
         "name": "Kawhi",
         "photo": "https://s3.amazonaws.com/sftb-images/wp-content/uploads/2017/03/30212224/Kawhi-Leonard.jpg",
         "scores": [
+            4,
+            5,
+            4,
             5,
             1,
-            4,
-            4,
-            5,
-            1,
-            2,
             5,
             4,
-            1
+            4,
+            3,
+            5
         ]
     },
     {
@@ -99,15 +99,15 @@ const friends = [{
         "photo": "http://static3.businessinsider.com/image/5911ce7473f2f31a008b4a53-480/draymond-green.jpg",
         "scores": [
             5,
-            1,
-            4,
-            4,
             5,
+            2,
+            4,
             1,
             2,
-            5,
-            4,
-            1
+            2,
+            3,
+            3,
+            4
         ]
     },
     {
@@ -115,31 +115,31 @@ const friends = [{
         "photo": "https://upload.wikimedia.org/wikipedia/commons/9/9e/Phil_Jackson_Lipofsky.JPG",
         "scores": [
             5,
-            1,
-            4,
-            4,
             5,
             1,
-            2,
-            5,
-            4,
-            1
+            3,
+            1,
+            3,
+            3,
+            1,
+            1,
+            2
         ]
     },
     {
         "name": "Mike",
         "photo": "http://www.slate.com/content/dam/slate/articles/sports/sports_nut/2017/05/170508_SNUT_MikeDAntoni.jpg.CROP.promo-xlarge2.jpg",
         "scores": [
+            3,
             5,
-            1,
-            4,
-            4,
-            5,
-            1,
+            3,
             2,
-            5,
             4,
-            1
+            3,
+            2,
+            1,
+            5,
+            2
         ]
     },
     {
@@ -147,15 +147,15 @@ const friends = [{
         "photo": "https://cdn-s3.si.com/s3fs-public/videos/2157889318001_3940840300001_Conley-beer.jpg",
         "scores": [
             5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
             5,
             4,
-            1
+            1,
+            4,
+            4,
+            2,
+            3,
+            5
         ]
     },
 
