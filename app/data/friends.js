5146f63ae2f9940ba3f43f4d5406ba7c913c439a
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-10 16:02:12 -0500

    added 10 friends to the friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,3 @@
-// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
-
-
-
-
-
 const friends = [{
         "name": "Lebron",
         "photo": "http://media.cleveland.com/cavs_impact/photo/lebron-james-media-dayjpg-df52d9ef28aa19de.jpg",
@@ -35,9 +29,140 @@ const friends = [{
             4,
             1
         ]
-    }
+    },
+    {
+        "name": "Giannis",
+        "photo": "https://clutchpoints.com/wp-content/uploads/2016/10/giannis-antetokounmpo.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Steph",
+        "photo": "http://betterlivingthroughbeowulf.com/wp-content/uploads/2013/05/Stephen-Curry.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Kevin",
+        "photo": "http://s1.firstpost.in/wp-content/uploads/2017/02/Kevin-Durant-NBA-vs-Thunder-AP.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Kawhi",
+        "photo": "https://s3.amazonaws.com/sftb-images/wp-content/uploads/2017/03/30212224/Kawhi-Leonard.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Draymond",
+        "photo": "http://static3.businessinsider.com/image/5911ce7473f2f31a008b4a53-480/draymond-green.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Phil",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/9/9e/Phil_Jackson_Lipofsky.JPG",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Mike",
+        "photo": "http://www.slate.com/content/dam/slate/articles/sports/sports_nut/2017/05/170508_SNUT_MikeDAntoni.jpg.CROP.promo-xlarge2.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Mike",
+        "photo": "https://cdn-s3.si.com/s3fs-public/videos/2157889318001_3940840300001_Conley-beer.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
 
 ];
+
+
 
 
 module.exports = friends;
