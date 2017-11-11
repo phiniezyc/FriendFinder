ae97c32ab8260ca779ffff321a776f7c62eb65cf
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-10 20:15:41 -0500

    cleaned up code to remove unnecessary code and comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,49 +1,6 @@
-//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
-
-
-// 6. Determine the user's most compatible friend using the following as a guide:
-
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//      * Example: 
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
-//    * The closest match will be the user with the least amount of difference.
-
-// 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-//    * The modal should display both the name and picture of the closest match. 
-
-//userData.scores ?
-
 
 const path = require("path");
 const friends = require("../data/friends");
-
-
-
-
-
-
-
-const friend1 = friends[0].scores; // gives us the first user's score and so forth
-const friend2 = friends[1].scores;
-const friend3 = friends[2].scores;
-const friend4 = friends[3].scores;
-const friend5 = friends[4].scores;
-const friend6 = friends[5].scores;
-const friend7 = friends[6].scores;
-const friend8 = friends[7].scores;
-const friend9 = friends[8].scores;
-const friend10 = friends[9].scores;
-
-
-
-
-
-
-
 
 
 
